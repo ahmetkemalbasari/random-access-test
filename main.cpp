f9e4e80fa64a71f58b3e0153a81be9f5f1e844a1
ahmetkemalbasari <akemalbasari@gmail.com>
2024-06-21 19:21:58 +0300

    fix infinite loop bug and improve performance

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 #include <cstring>
+#include <filesystem>
 #include <vector>
 #include <sstream>
 #include <fstream>
 #include <sys/stat.h>
 
-//FILE OPERATIONS
-FILE *dataFile = fopen("../data.dat", "w+");
-FILE *indexFile = fopen("../index.dat", "w+");
+#define NODE_SIZE (int) sizeof(Node)
+#define STUDENT_SIZE (int) sizeof(Student)
 
-int sizeOf(const std::string &path) {
+#define INDEX_FILE_COUNT 100000
+#define INDEX_FILE_SIZE (STUDENT_SIZE * INDEX_FILE_COUNT)
+
+
+//FILE OPERATIONS
+FILE *dataFile;
+FILE *indexFile;
+
+int fileSize(const std::string &path) {
     struct stat file_stat{};
     stat(path.data(), &file_stat);
     return static_cast<int>(file_stat.st_size);
@@ -44,85 +52,68 @@ struct Node {
 
 // DATA OPS
 int currentDataIndex;
-int currentOverloadIndex;
+int currentOverflowIndex;
 
-int hashF(int id) {
-    return (id % 100000);
+int hash(int id) {
+    return (id % INDEX_FILE_COUNT);
 }
 
 Student searchStudent(int id) {
-    int hashI = hashF(id);
-    Node curNode{-1, -1, -1};
-    Student student{0, 0, 0, false, ""};
+    int homeAddress = hash(id);
 
-    fseek(indexFile, hashI * sizeof(Node), SEEK_SET);
-    fread(&curNode, sizeof(Node), 1, indexFile);
+    Node curNode{};
+    fseek(indexFile, homeAddress * NODE_SIZE, SEEK_SET);
+    fread(&curNode, NODE_SIZE, 1, indexFile);
 
-    //printf("searchNode: %d id, %d next node\n", curNode.id, curNode.nextNode);
-    if (curNode.id == -1)
-        return student;
+    if (curNode.id == 0 || curNode.id == -1)
+        return {};
 
     while (true) {
-        if (curNode.id == id) {
+        if (curNode.id == id)
             break;
-        } else if (curNode.nextNode == -1)
-            return student;
+        if (curNode.nextNode == -1)
+            return {};
 
-        printf("curNode: %d id, %d next node\n", curNode.id, curNode.nextNode);
-        fseek(indexFile, (100000 + curNode.nextNode) * sizeof(Node), SEEK_SET);
+        fseek(indexFile, (curNode.nextNode + INDEX_FILE_COUNT) * NODE_SIZE, SEEK_SET);
         fread(&curNode, sizeof(Node), 1, indexFile);
-
-        printf("curNode: %d id, %d next node\n", curNode.id, curNode.nextNode);
     }
-    fseek(dataFile, curNode.dataIndex * sizeof(Student), SEEK_SET);
-    fread(&student, sizeof(Student), 1, dataFile);
+
+    Student student{};
+    fseek(dataFile, STUDENT_SIZE * curNode.dataIndex, SEEK_SET);
+    fread(&student, STUDENT_SIZE, 1, dataFile);
 
     return student;
 }
 
-bool iDExists(int id) {
-    Student student = searchStudent(id);
-
-    if (student.id != 0)
-        return true;
-
-    return false;
+bool existsStudent(int id){
+    return searchStudent(id).id != 0;
 }
 
 void addStudent(Student student) {
-    int hashI = hashF(student.id);
-    Node curIndex{-1, -1, -1};
+    int homeAddress = hash(student.id);
     Node newIndex{student.id, -1, currentDataIndex};
 
-    fseek(dataFile, sizeof(Student) * currentDataIndex, SEEK_SET);
-    fwrite(&student, sizeof(Student), 1, dataFile);
+    fseek(dataFile, STUDENT_SIZE * currentDataIndex++, SEEK_SET);
+    fwrite(&student, STUDENT_SIZE, 1, dataFile);
 
-    fseek(indexFile, hashI * sizeof(Node), SEEK_SET);
+    Node curIndex{};
+    fseek(indexFile, homeAddress * NODE_SIZE, SEEK_SET);
     fread(&curIndex, sizeof(Node), 1, indexFile);
-    currentDataIndex++;
 
-    printf("///// home: %d id, %d nextIndex \n", curIndex.id, curIndex.nextNode);
-
-    if (curIndex.id == -1) {
-        fseek(indexFile, hashI * sizeof(Node), SEEK_SET);
+    if (curIndex.id == 0) {
+        fseek(indexFile, homeAddress * NODE_SIZE, SEEK_SET);
         fwrite(&newIndex, sizeof(Node), 1, indexFile);
-        printf("newHome: %d id, %d nextIndex \n", newIndex.id, newIndex.nextNode);
         return;
     }
 
     newIndex.nextNode = curIndex.nextNode;
-    curIndex.nextNode = currentOverloadIndex;
+    curIndex.nextNode = currentOverflowIndex++;
 
-    fseek(indexFile, sizeof(Node) * (currentOverloadIndex + 100000), SEEK_SET);
+    fseek(indexFile, (currentOverflowIndex + INDEX_FILE_COUNT - 1) * NODE_SIZE, SEEK_SET);
     fwrite(&newIndex, sizeof(Node), 1, indexFile);
 
-    fseek(indexFile, hashI * sizeof(Node), SEEK_SET);
+    fseek(indexFile, homeAddress * NODE_SIZE, SEEK_SET);
     fwrite(&curIndex, sizeof(Node), 1, indexFile);
-
-    printf("newIndex: %d id, %d nextIndex ///// home: %d id, %d nextIndex \n", newIndex.id, newIndex.nextNode,
-           curIndex.id, curIndex.nextNode);
-    currentOverloadIndex++;
-    return;
 }
 
 Student convertToStudent(const std::string &string) {
@@ -139,218 +130,58 @@ Student convertToStudent(const std::string &string) {
     student.gender = splitted[2] == "Kadın";
     student.age = stoi(splitted[3]);
     student.grade = stoi(splitted[4]);
-    memcpy(&student.name, &splitted[1], 32);
+    memcpy(&student.name, splitted[1].c_str(), 32);
 
     return student;
 }
 
 
 int main() {
+    if (!std::filesystem::exists("../data.dat"))
+        std::ofstream index("../data.dat");
+    if (!std::filesystem::exists("../index.dat"))
+        std::ofstream index("../index.dat");
+    if (std::filesystem::file_size("../index.dat") < INDEX_FILE_SIZE)
+        std::filesystem::resize_file("../index.dat", INDEX_FILE_SIZE);
 
-    Node indexData{-1, -1, -1};
-    for (int i = 0; i < 100000; i++) {
-        fwrite(&indexData, sizeof(Node), 1, indexFile);
-    }
+    dataFile = fopen("../data.dat", "rb+");
+    indexFile = fopen("../index.dat", "rb+");
 
-    //currentDataIndex = sizeOf("../data.dat") / sizeof(Student);
-    //currentOverloadIndex = (sizeOf("../index.dat") / sizeof(Node)) - 100000;
-    currentDataIndex = 0;
-    currentOverloadIndex = 0;
+    currentDataIndex = fileSize("../data.dat") / STUDENT_SIZE;
+    currentOverflowIndex = fileSize("../index.dat") / NODE_SIZE - 100000;
 
-
-    int i = 0;
     for (std::string line: readLines("../students.txt")) {
         Student student = convertToStudent(line);
-        printf("%d \n", i);
-        if (!iDExists(student.id)) {
+
+        if(!existsStudent(student.id))
             addStudent(student);
-        }
-        if (++i == 1000000)
-            break;
     }
 
+    srand((unsigned)time(0));
+    int n;
+
+    std::vector<Student> students;
+    for(int j = 0; j < 500000; j++){
+        Student student;
+        n = (rand()%10000000);
+
+        fseek(dataFile, STUDENT_SIZE*n, SEEK_SET);
+        fread(&student, STUDENT_SIZE, 1, dataFile);
+
+        students.push_back(student);
+    }
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for(Student s : students)
+        searchStudent(s.id);
+
+    auto finish = std::chrono::high_resolution_clock::now();
+    std::cout << "\nAverage seek time:" <<std::chrono::duration_cast<std::chrono::nanoseconds>(finish-start).count() / 1000000.0 << "ms\n";
+
+
+    Student student = searchStudent(550362344);
+    printf("%d %s %d %d\n", student.id, student.name, student.age, student.grade);
 
     return 0;
 }
-/*
-    IndexData indexData{-1, -1, -1};
-    for (int i = 0; i < 100000; i++) {
-        fwrite(&indexData, sizeof(IndexData), 1, indexFile);
-    }
-
-*/
-
-/*
- #include <iostream>
-#include <cstring>
-#include <vector>
-#include <sstream>
-#include <fstream>
-#include <sys/stat.h>
-
-//FILE OPERATIONS
-FILE *dataFile = fopen("../data.dat", "rb+");
-FILE *indexFile = fopen("../index.dat", "rb+");
-
-int sizeOf(const std::string &path) {
-    struct stat file_stat{};
-    stat(path.data(), &file_stat);
-    return static_cast<int>(file_stat.st_size);
-}
-
-std::vector<std::string> readLines(const std::string &path) {
-    std::vector<std::string> lines;
-    std::ifstream file(path);
-    std::string str;
-    while (std::getline(file, str))
-        lines.push_back(str);
-
-    return lines;
-}
-
-
-//STRUCTS
-struct Student {
-    int id;
-    char age;
-    char grade;
-    bool gender;
-    char name[32];
-};
-
-struct IndexData {
-    int id;
-    int next;
-    int index;
-};
-
-
-//DATA OPERATIONS
-int currentData;
-int currentIndex;
-
-int hash(int id) {
-    return id % 100000;
-}
-
-bool checkIndex(int id) {
-    int home = hash(id);
-    IndexData indexData{};
-
-    fseek(indexFile, home * sizeof(IndexData), SEEK_SET);
-    fread(&indexData, sizeof(IndexData), 1, indexFile);
-
-    while (id != indexData.id){
-        home = indexData.next;
-        fseek(indexFile, (home + 100000) * sizeof(IndexData), SEEK_SET);
-        fread(&indexData, sizeof(IndexData), 1, indexFile);
-
-        if(indexData.next == -1)
-            break;
-    }
-
-    return indexData.id != -1;
-}
-
-void saveStudent(Student student) {
-    fseek(dataFile, 0, SEEK_SET);
-    fwrite(&student, sizeof(Student), 1, dataFile);
-
-    IndexData newIndex{student.id, -1, currentData};
-    IndexData indexData{};
-
-    int home = hash(student.id);
-
-    fseek(indexFile, home * sizeof(IndexData), SEEK_SET);
-    fread(&indexData, sizeof(IndexData), 1, indexFile);
-
-    if (indexData.id == -1) {
-        fseek(indexFile, home * sizeof(IndexData), SEEK_SET);
-        fwrite(&newIndex, sizeof(IndexData), 1, indexFile);
-        return;
-    }
-
-    newIndex.next = indexData.next;
-    indexData.next = currentIndex++;
-
-    fseek(indexFile, (currentIndex + 99999) * sizeof(IndexData), SEEK_SET);
-    fwrite(&newIndex, sizeof(IndexData), 1, indexFile);
-
-    fseek(indexFile, home * sizeof(IndexData), SEEK_SET);
-    fwrite(&indexData, sizeof(IndexData), 1, indexFile);
-
-}
-
-Student searchStudent(int id) {
-    int home = hash(id);
-    IndexData indexData{};
-    fseek(indexFile, home * sizeof(IndexData), SEEK_SET);
-    fread(&indexData, sizeof(IndexData), 1, indexFile);
-    while (id != indexData.id){
-        home = indexData.next;
-        fseek(indexFile, (home + 100000) * sizeof(IndexData), SEEK_SET);
-        fread(&indexData, sizeof(IndexData), 1, indexFile);
-
-        if(indexData.next == -1)
-            break;
-    }
-
-    if (indexData.id == -1)
-        return Student{};
-
-    Student student{};
-
-    fseek(dataFile, indexData.index * sizeof(Student), SEEK_SET);
-    fread(&student, sizeof(Student), 1, dataFile);
-    return student;
-}
-
-Student convertToStudent(const std::string &string) {
-    Student student{0, 0, 0, false, ""};
-
-    std::vector<std::string> splitted;
-    std::string token;
-    std::istringstream tokenStream(string);
-    while (std::getline(tokenStream, token, '|')) {
-        splitted.push_back(token);
-    }
-
-    student.id = stol(splitted[0]);
-    student.gender = splitted[2] == "Kadın";
-    student.age = stoi(splitted[3]);
-    student.grade = stoi(splitted[4]);
-    memcpy(&student.name, &splitted[1], 32);
-
-    return student;
-}
-
-
-int main() {
-    currentData = sizeOf("../data.dat") / sizeof(Student);
-    currentIndex = sizeOf("../index.dat") / sizeof(IndexData) - 100000;
-
-
-    int i = 0;
-    for (std::string line: readLines("../students.txt")) {
-        Student student = convertToStudent(line);
-        if (!checkIndex(student.id)) {
-            saveStudent(student);
-        }
-
-        if (++i == 1000000)
-            break;
-    }
-
-
-    Student s = searchStudent(782014280);
-    printf("%d, %s", s.id, s.name);
-
-    return 0;
-}
-/*
-    IndexData indexData{-1, -1, -1};
-    for (int i = 0; i < 100000; i++) {
-        fwrite(&indexData, sizeof(IndexData), 1, indexFile);
-    }
-
-*/
